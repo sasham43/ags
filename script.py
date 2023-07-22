b8319bfa77cbc7899f9648229b16deafa8b56795
Sasha Kramer <sashasemail@gmail.com>
2023-07-22 13:26:42 -0400

    add button library

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
-# from gpiozero import Button
-# button = Button(21)
+from gpiozero import Button
 import os
 import subprocess
 
@@ -10,16 +9,43 @@ index = 0
 
 vids = os.listdir(script_dir)
 # print(os.listdir(script_dir))
-current_vid = vids[index]
+# current_vid = vids[index]
 
-# command = 'omxplayer --no-osd --loop {0}'.format(current_vid)
-command = [
-    'omxplayer',
-    '--no-osd',
-    '--loop',
-    '{0}/{1}'.format(script_dir, current_vid)
-]
+# # command = 'omxplayer --no-osd --loop {0}'.format(current_vid)
+# command = [
+#     'omxplayer',
+#     '--no-osd',
+#     '--loop',
+#     '{0}/{1}'.format(script_dir, current_vid)
+# ]
 
-sp = subprocess.run(command)
+# sp = subprocess.run(command)
 
-print("The exit code was: %d" % sp.returncode)
+# print("The exit code was: %d" % sp.returncode)
+
+
+def on_press():
+    index = index + 1
+    run_cmd()
+
+def run_cmd():
+    current_vid = vids[index]
+
+    # command = 'omxplayer --no-osd --loop {0}'.format(current_vid)
+    command = [
+        'omxplayer',
+        '--no-osd',
+        '--loop',
+        '{0}/{1}'.format(script_dir, current_vid)
+    ]
+
+    sp = subprocess.run(command)
+
+    print("The exit code was: %d" % sp.returncode)
+
+
+button = Button(21)
+
+button.when_pressed = on_press
+
+run_cmd()
