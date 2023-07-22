014f274cf5ca137501bfff64b894803fa5d92769
Sasha Kramer <sashasemail@gmail.com>
2023-07-22 13:14:04 -0400

    add script to list files

---
+++ script.py
@@ -0,0 +1,18 @@
+# from gpiozero import Button
+# button = Button(21)
+import os
+
+# script_dir = '/home/pi/vids'
+script_dir = '/Users/sashakramer/ags-vids'
+
+index = 0
+
+vids = os.listdir(script_dir)
+# print(os.listdir(script_dir))
+current_vid = vids[index]
+
+command = 'omxplayer --no-osd --loop {0}'.format(current_vid)
+
+subprocess = os.subprocess(command)
+
+print("The exit code was: %d" % subprocess.returncode)
